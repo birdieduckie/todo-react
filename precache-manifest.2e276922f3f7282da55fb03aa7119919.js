02758d8b17c79eb2c2ab704d2d6b0ce5816327c1
Natasha S <vrthngcnts@gmail.com>
2020-06-02 21:06:54 +0300

    Updates

---
+++ precache-manifest.2e276922f3f7282da55fb03aa7119919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf33765407136f40111b5ce325a9322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1654c4715155729b3246",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2432b6d3432f10f95d35",
+    "url": "/static/css/main.79583c7a.chunk.css"
+  },
+  {
+    "revision": "1654c4715155729b3246",
+    "url": "/static/js/2.37e8428d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.37e8428d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2432b6d3432f10f95d35",
+    "url": "/static/js/main.e1618edd.chunk.js"
+  },
+  {
+    "revision": "96dd86ef88d7bc22bbd1",
+    "url": "/static/js/runtime-main.c21d19b4.js"
+  }
+]);
